94735c6aad5031a9e8c8693f8977cfd93f9a3635
Ana Coelho <70607969+anabcoelho@users.noreply.github.com>
2022-11-07 21:38:15 -0300

    Update streamlit_app.py
    arrumando nosso filtro

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,9 @@ lista_fruta = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw
 lista_fruta = lista_fruta.set_index('Fruit')
 
 # colocar um filtrozin
-sl.multiselect("escolhe a fruta:", list(lista_fruta.index),['Avocado','Strawberries'])
+meufiltro = sl.multiselect("escolhe a fruta:", list(lista_fruta.index),['Avocado','Strawberries'])
 
+mostra_pramim = lista_fruta.loc[meufiltro]
 # Display the table on the page.
 
-sl.dataframe(lista_fruta)
+sl.dataframe(mostra_pramim)
