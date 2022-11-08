229a5b30b5699343fd0705b1e381a8d7df4a8121
Ana Coelho <70607969+anabcoelho@users.noreply.github.com>
2022-11-07 21:19:33 -0300

    Update streamlit_app.py
    vamos melhorar as piadas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,14 @@ import streamlit as sl
 
 sl.title('O jantar po')
 
-sl.header('Menu!')
+sl.header('piu!')
 sl.text('🥣 Qual o cereal favorito do vampiro? A veia')
 sl.text('🥗 Qual a vitamina mais engraçada? A vitamina KKKKKKKKKKKK')
 sl.text('🐔 Ovin ou a galin')
-sl.text('''🥑	
-Num hospicio, numa bela tarde um grupo de loucos sem nada pra fazer resolvem todos brincar de abacate maduro, 
-aquele imenso pé de manga depois de segundos se tornou um grande pé de abacate maduros onde os loucos ficavam de ponta cobeça.
-Um dos doidos que por la embaixo ficou arremeçando padras que os derrubava, um inssistente que ja tinha levado mais de 30 pedradas teimava estar verde, 
-o que arremessava subiu lá em sima do pé e enfiou seu dedo no cú do teimaso e disse "VOCÊ ESTÁ É PODRE" ''')
+sl.text('''Duas formigas se encontraram e pararam para conversar:
+- Oi, qual é o seu nome?
+- Fu.
+- Fu o quê?
+- Fu Miga!''')
 
 sl.header('🥝🍇🍌🥭 boracozinhar 🥝🍇🍌🥭')
