d34f4030e22e0d26996fc4a154a22e83c4748589
Ana Coelho <70607969+anabcoelho@users.noreply.github.com>
2022-11-07 21:11:11 -0300

    Update streamlit_app.py
    traduzindo o app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as sl
 
-sl.title('my parents new healthy dinner')
+sl.title('O jantar po')
 
-sl.header('Gourmet egg')
-sl.text('boiled egg')
+sl.header('Menu!')
+sl.text('🥣 Qual o cereal favorito do vampiro? A veia')
+sl.text('🥗 Qual a vitamina mais engraçada? A vitamina KKKKKKKKKKKK')
+sl.text('🐔 Ovin ou a galin')
+sl.text('''🥑	
+Num hospicio, numa bela tarde um grupo de loucos sem nada pra fazer resolvem todos brincar de abacate maduro, 
+aquele imenso pé de manga depois de segundos se tornou um grande pé de abacate maduros onde os loucos ficavam de ponta cobeça.
+Um dos doidos que por la embaixo ficou arremeçando padras que os derrubava, um inssistente que ja tinha levado mais de 30 pedradas teimava estar verde, 
+o que arremessava subiu lá em sima do pé e enfiou seu dedo no cú do teimaso e disse "VOCÊ ESTÁ É PODRE" ''')
+
+sl.header('🥝🍇🍌🥭 boracozinhar 🥝🍇🍌🥭')
